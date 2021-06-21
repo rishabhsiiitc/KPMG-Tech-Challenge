4152568345c6fb9ee24ac52509b8de48a9cd4a9c
rishabhsiiitc <85956674+rishabhsiiitc@users.noreply.github.com>
2021-06-21 13:22:06 +0100

    Update Function.js

--- Challenge 3/Solution/Code/Function.js
+++ Challenge 3/Solution/Code/Function.js
@@ -1,1 +1,36 @@
-Hello World!
+///**Solution using Javascript**///
+
+///***Without using any library, Used Object.entries***//
+
+let obj = {
+    a1: {
+        b1: {
+            nested: true
+        },
+    },
+    x1: {
+        y1: 'zl',
+    }
+};
+
+
+function getObjectKeys(object, key) {
+    // if you want another split key you can change this.
+    const keys = key.split('.');
+    let obj = object;
+    for (let ikey of keys) {
+        for (let [objKey, value] of Object.entries(obj)) {
+            if(!keys.includes(objKey)) {
+                continue;
+            }
+            obj = value;
+        }
+    }
+    return obj;
+}
+
+console.log(getObjectKeys(obj, 'a1.b1.nested'));
+
+console.log(getObjectKeys(obj, 'x1.y1'));
+// even if there is another additional key
+console.log(getObjectKeys(obj, 'x1.y1.nested'));
